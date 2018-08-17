5d58194e9dcb59cace1a208683c5fcfe1de687fc
Popeye985 <ceulemans_koen@hotmail.com>
2018-08-17 13:23:01 +0200

    Initial commit

---
+++ src/com/company/Winkelmandje.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Winkelmandje {
+    public void addProduct(String naam, int prijs) {
+    }
+
+    public int getAantalProducten() {
+        return 0;
+    }
+}
